cba7b4d7393eed7ceef184c6154e6eb612cd6fa2
Kert <kert2005@gmail.com>
2022-07-26 16:43:39 +0300

    Added password choices
    Can now choose between lowercase, uppercase, numbers, symbols.

--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,7 @@
 
 import random as r
+from settings import *
+
 
 print("""
  ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄
@@ -13,34 +15,111 @@ print("""
 made by Kert252
 """)
 
-lower = 'abcdefghijklmnopqrstuvwxyz'
-upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-num = '0123456789'
-sym = '*@#%?!$&^'
-
-lenght = int(input("Password lenght: "))
-amount = int(input("Amount of passwords: "))
+hm = ['settings.lEnabled', 'settings.uEnabled',
+'settings.numEnabled', 'settings.symEnabled']
 
 
-def createPas():
-    pas = ""
-    for j in range(1, amount+1):
-        for i in range(lenght):
-            x = r.randint(1, 4)
-            if x == 1:
-                y = r.choice(lower)
-                pas = pas + y
-            elif x == 2:
-                y = r.choice(upper)
-                pas = pas + y
-            elif x == 3:
-                y = r.choice(num)
-                pas = pas + y
-            elif x == 4:
-                y = r.choice(sym)
-                pas = pas + y
-        print(f"{j}: {pas}")
-        pas = ""
+def generate(lh):
+    print("\nPasswords generated: ")
+    pref = ""
+    for j in hm:
+        if j == 'settings.lEnabled' and eval(j) == True:
+            pref = pref + lower
+        if j == 'settings.uEnabled' and eval(j) == True:
+            pref = pref + upper
+        if j == 'settings.numEnabled' and eval(j) == True:
+            pref = pref + num
+        if j == 'settings.symEnabled' and eval(j) == True:
+            pref = pref + sym
+    pref = list(pref)
+    for i in range(lh[1]):
+        password = []
+        r.shuffle(pref)
+        for i in range(lh[0]):
+            password.append(r.choice(pref))
+        r.shuffle(password)
+        print("".join(password))
 
 
-createPas()
+def printOp():
+    print(f"""
+    1. [Generate passwords]
+    2. lowercase = {bool(settings.lEnabled)}
+    3. uppercase = {settings.uEnabled}
+    4. numbers = {settings.numEnabled}
+    5. symbols = {settings.symEnabled}
+    """)
+
+
+def choice():
+    while True:
+        try:
+            choice = int(input("Enter a number: "))
+            if choice >= 1 and choice <= 5:
+                if choice == 1:
+                    check = False
+                    for i in hm:
+                        if eval(i) == True:
+                            check = True
+                    if check == True:
+                        break
+                    else:
+                        print("Enable at least 1 option")
+                if choice == 2:
+                    if bool(settings.lEnabled) == True:
+                        settings.lEnabled = False
+                        printOp()
+                    else:
+                        settings.lEnabled = True
+                        printOp()
+                if choice == 3:
+                    if bool(settings.uEnabled) == True:
+                        settings.uEnabled = False
+                        printOp()
+                    else:
+                        settings.uEnabled = True
+                        printOp()
+                if choice == 4:
+                    if bool(settings.numEnabled) == True:
+                        settings.numEnabled = False
+                        printOp()
+                    else:
+                        settings.numEnabled = True
+                        printOp()
+                if choice == 5:
+                    if bool(settings.symEnabled) == True:
+                        settings.symEnabled = False
+                        printOp()
+                    else:
+                        settings.symEnabled = True
+                        printOp()
+            else:
+                print("Please enter a valid number! \n")
+        except ValueError:
+            print("Please enter a valid integer! \n")
+
+
+def choice2():
+    while True:
+        try:
+            lenght = int(input("Password lenght: "))
+            break
+        except ValueError:
+            print("Please enter an integer! \n")
+
+    while True:
+        try:
+            amount = int(input("Amount of passwords: "))
+            break
+        except ValueError:
+            print("Please enter an integer! \n")
+
+    return [lenght, amount]
+
+
+while True:
+    printOp()
+    choice()
+    print("")
+    generate(choice2())
+    input("\nHit any key to continue. ")
---
+++ settings.py
@@ -0,0 +1,17 @@
+
+# Base settings
+
+class settings:
+    lEnabled = True
+    uEnabled = True
+    numEnabled = True
+    symEnabled = True
+
+
+# Change as you wish
+# Symbols and numbers are doubled for better ratio
+
+lower = 'abcdefghijklmnopqrstuvwxyz'  # 26
+upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'  # 26
+num = '01234567890123456789'  # 20
+sym = '*@#%?!$&*@#%?!$&'  # 16
